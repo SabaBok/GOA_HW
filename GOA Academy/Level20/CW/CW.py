977448e5d8201dc61aee59570b083183b8a2fe3f
SabaBok <saba.bokuchava555@gmail.com>
2024-10-28 22:14:05 +0400

    Create CW.py

---
+++ GOA Academy/Level20/CW/CW.py
@@ -0,0 +1,17 @@
+# list = ["luka lortqipoanidze","gabriel molodini", "beqa berashvili", "giorgi cxadaze" 
+#         ,"giorgi jankhoteli", "irakli somkhishvili", 
+#         "mariam jalahgonia", "nani matiashvili","nikoloz bagaturia", 
+#         "sopia gorgadze", "aleko gorjomeladze","davit narimanidze","giorgi khucishvili"]
+# new_list = []
+
+# def PrintNames(letter):
+#         for i in list:
+#                 if i[0] == letter:
+#                         new_list.append(i)
+        
+#         print(new_list)
+
+# PrintNames("g")
+
+
+
