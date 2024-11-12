556bbaccd06d4fcbc1a9755c4004476703ff61d4
SabaBok <saba.bokuchava555@gmail.com>
2024-11-12 17:34:34 +0400

    commit

--- GOA Academy/Level26/CW/CW1.py
+++ GOA Academy/Level26/CW/CW1.py
@@ -1,4 +1,20 @@
-list = [1,2,3,4,5,6]
-list.insert(3,"kikop")
-print(list)
+name_list = ["gabrieli","levani","giorgi","saba"]
+name2_list = ["saba","nika","luka"]
 
+# name_list.extend(name2_list)
+# print(name_list)
+
+# name_list.insert(5,"saba")
+# print(name_list)
+# print(len(name_list))
+# print(name_list.count("saba"))
+
+# print(name_list.index("saba"))
+#^
+#| 
+# index = 0
+# for i in name_list:
+#     if i == "saba":
+#         print(index)
+#     else:
+#         index += 1
