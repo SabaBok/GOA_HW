6419cf2644a188262082776dc5be9a4592f58e18
SabaBok <saba.bokuchava555@gmail.com>
2024-12-08 13:36:05 +0400

    commit

--- GOA Academy/Level35/HW/HW11.py
+++ GOA Academy/Level35/HW/HW11.py
@@ -1,8 +1,7 @@
 #11) https://www.codewars.com/kata/56676e8fabd2d1ff3000000c/train/python 
 def find_needle(haystack):
-    count = -1
+    counts = -1
     for i in haystack:
-        count +=1
+        counts +=1
         if i == "needle":
-            return f"found the needle at position {count}"
-    return "there is no needle"
+            return f"found the needle at position {counts}"
