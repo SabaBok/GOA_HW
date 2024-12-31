a6eb35f856076b769cc11d528ea1e2182407b88f
SabaBok <saba.bokuchava555@gmail.com>
2024-12-31 22:10:46 +0400

    commit

--- GOA Academy/Level45/CW/CW1.py
+++ GOA Academy/Level45/CW/CW1.py
@@ -1,1 +1,8 @@
 # 1) https://www.codewars.com/kata/631f0c3a0b9cb0de6ded0529/train/python
+def evaluate(equation):
+    a, *c = map(int, equation.split("@"))
+    for b in c:
+        if b == 0:
+            return
+        a = a * (b + 2) + a // b
+    return a
--- GOA Academy/Level45/HW/HW1.py
+++ GOA Academy/Level45/HW/HW1.py
@@ -1,4 +1,3 @@
 #1)https://www.codewars.com/kata/586f6741c66d18c22800010a
-
 def sequence_sum(start, end, step):
     return sum(range(start, end+1, step))
