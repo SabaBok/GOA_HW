8824555118d2fb307ae155abcd3c8bb49250e135
SabaBok <saba.bokuchava555@gmail.com>
2025-01-05 17:01:18 +0400

    commit

--- GOA Academy/Level45/HW/HW1.py
+++ GOA Academy/Level45/HW/HW1.py
@@ -1,3 +1,6 @@
 #1)https://www.codewars.com/kata/586f6741c66d18c22800010a
-def sequence_sum(start, end, step):
-    return sum(range(start, end+1, step))
+def sequence_sum(begin, end, step):
+    if begin > end:
+        return 0
+    else:
+        return begin + (sequence_sum(begin + step, end, step))
