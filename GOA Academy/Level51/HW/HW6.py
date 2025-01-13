a98921a8895849a311d4d8ddf35e3965611a451c
SabaBok <saba.bokuchava555@gmail.com>
2025-01-13 22:26:12 +0400

    commit

--- GOA Academy/Level51/HW/HW6.py
+++ GOA Academy/Level51/HW/HW6.py
@@ -2,7 +2,11 @@
 def find_even_index(arr):
     ind = arr[0]   
     for i in arr:
-        if sum(arr[0:arr.index(i)]) == sum(arr[arr.index(i):-1]):
+        if sum(arr[0:arr.index(i)-1]) == sum(arr[arr.index(i)-1:-1]):
             ind = arr.index(i)
-
     return ind
+
+arr = [1,2,3,4,3,2,1]
+print(find_even_index(arr))
+
+
