29316fb9e44e10c8d95c3b8ac4dd20003765862a
SabaBok <saba.bokuchava555@gmail.com>
2025-01-23 20:10:30 +0400

    commit

--- GOA Academy/Level51/HW/HW2.py
+++ GOA Academy/Level51/HW/HW2.py
@@ -1,9 +1,11 @@
 #2)https://www.codewars.com/kata/580878d5d27b84b64c000b51
 def sum_triangular_numbers(n):
-    if n < 0:
-        return 0
-    total = 0
-    for i in range(1, n + 1):
-        triangular_number = (i * (i + 1)) // 2
-        total += triangular_number
-    return total
+    my_int = 0
+    sum = 0
+    for i in range(1,n+1):
+        sum +=i
+        my_int+=sum
+    return my_int
+
+# print(sum_triangular_numbers(6))
+
--- GOA Academy/Level51/HW/HW4.py
+++ GOA Academy/Level51/HW/HW4.py
@@ -1,9 +1,10 @@
 #4)https://www.codewars.com/kata/554ca54ffa7d91b236000023
 def delete_nth(order, max_e):
-    result = []
-    counts = {}
-    for num in order:
-        if counts.get(num, 0) < max_e:
-            result.append(num)
-            counts[num] = counts.get(num, 0) + 1
-    return result
+
+    new_list = []
+    for i in order:
+        if new_list.count(i) >= max_e:
+            continue
+        else:
+            new_list.append(i)
+    return new_list
