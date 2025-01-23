8c861692a3d62a543e862b91bad8d79e58ec64c6
SabaBok <saba.bokuchava555@gmail.com>
2025-01-24 00:52:26 +0400

    commit

---
+++ GOA Academy/Level56/HW/hw1.py
@@ -0,0 +1,9 @@
+#1)https://www.codewars.com/kata/593b1909e68ff627c9000186
+def nickname_generator(name):
+    if len(name) <= 3:
+        return "Error: Name too short"
+    vowels = "aeiou"
+    if name[2] in vowels:
+        return name[0:4]
+    else:
+        return name[0:3]
---
+++ GOA Academy/Level56/HW/hw2.py
@@ -0,0 +1,20 @@
+#2)https://www.codewars.com/kata/58afce23b0e8046a960000eb
+def withdraw(n):
+    hundreed = 0
+    fifty = 0
+    twenty = 0
+    if n%100 == 0:
+        hundreed += n/100
+        n -= hundreed*100
+        print(f"{hundreed} hundreed removed")
+    elif n%50 == 0:
+        fifty += n/50
+        n -=fifty*50
+        print(f"{fifty} fifty removed")
+    elif n%20 == 0:
+        twenty += n/20
+        n -=twenty*20
+        print(f"{twenty} twenty removed")
+    return [int(hundreed),int(fifty),int(twenty)]
+
+print(withdraw(250))
