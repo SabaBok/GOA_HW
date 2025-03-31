322d57f91700bb263341f614148b7b70bdbc298c
SabaBok <saba.bokuchava555@gmail.com>
2025-03-31 15:50:31 +0400

    commit

---
+++ GOA Academy/Level83/HW/script.js
@@ -0,0 +1,19 @@
+let but = document.querySelector("button")
+let body = document.body
+let text = document.querySelector("p").querySelector("span")
+let sound = document.querySelector("audio")
+but.addEventListener("click", function(){
+    let idk = "0123456789ABCDEF"
+    let color = ""
+
+    for(let i =0; i<6;i++){
+        let random = Math.floor(Math.random() * 16)
+        color += idk[random]
+    }
+    body.style.backgroundColor = `#${color}`
+    text.textContent = `#${color}`
+    text.style.color = `#${color}`
+
+    sound.play()
+    sound.pause
+})
