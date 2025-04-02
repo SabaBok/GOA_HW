5ccff536ce13ed70a2a479620e8b64fe83595751
SabaBok <saba.bokuchava555@gmail.com>
2025-04-02 15:46:10 +0400

    commit

---
+++ GOA Academy/Level84/HW/hw2/hw2.js
@@ -0,0 +1,23 @@
+let dino = document.querySelector("#dino")
+let cacti = document.querySelector("#cactus")
+function jump(){
+    if(dino.classList != "jump"){
+        dino.classList.add("jump")
+        setTimeout(function(){
+            dino.classList.remove("jump")
+        }, 300)
+    }
+}
+let isAlive = setInterval(function(){
+    let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"))
+    let cactusTop = parseInt(window.getComputedStyle(cacti).getPropertyValue("left"))
+    if(cactusTop <150 && cactusTop>100 && dinoTop >=140){
+        alert("You Have Lost")
+    }
+},10)
+document.addEventListener("keyup", function(e){
+    if(e.key == " "){
+        jump()
+    }
+    console.log(e.key)
+})
