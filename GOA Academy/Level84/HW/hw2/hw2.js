bcc4855c8e7aa4ab76b48087a04186ca7c7aed99
SabaBok <saba.bokuchava555@gmail.com>
2025-04-03 18:08:43 +0400

    commit

--- GOA Academy/Level84/HW/hw2/hw2.js
+++ GOA Academy/Level84/HW/hw2/hw2.js
@@ -1,40 +1,68 @@
 let dino = document.querySelector("#dino");
 let gameContainer = document.querySelector(".cont");
-let high = document.querySelector("#high")
-let cur = document.querySelector("#cur")
-high.textContent = localStorage.getItem("high")
+let high = document.querySelector("#high");
+let cur = document.querySelector("#cur");
+high.textContent = localStorage.getItem("high");
+let counter = 0;
+let but = document.querySelectorAll(".but");
+let gameSpeed = 60;
+let jumpSpeed = 300;
+let cactusSpeed = 2000;
 
+for (let i of but) {
+    i.addEventListener("click", function () {
+        if (this.textContent == "Easy") {
+            gameSpeed = 2000;
+            jumpSpeed = 450;
+            cactusSpeed = 2500;
+        } else if (this.textContent == "Normal") {
+            gameSpeed = 50;
+            jumpSpeed = 300;
+            cactusSpeed = 2000;
+        } else {
+            gameSpeed = 10;
+            jumpSpeed = 150;
+            cactusSpeed = 1000;
+        }
 
-let counter = 0
-setInterval(function(){
-    counter++
-    cur.textContent = counter
-},60)
+        dino.style.animationDuration = `${jumpSpeed / 1000}s`;
+
+        location.reload();
+    });
+}
+
+setInterval(function () {
+    counter++;
+    cur.textContent = counter;
+}, gameSpeed);
 
 function jump() {
     if (!dino.classList.contains("jump")) {
         dino.classList.add("jump");
-        setTimeout(function(){
+        dino.style.animation = `jump ${jumpSpeed / 1000}s linear`;
+
+        setTimeout(function () {
             dino.classList.remove("jump");
-        }, 300);
+        }, jumpSpeed);
     }
 }
 
 function spawnCactus() {
     let cactus = document.createElement("div");
     cactus.classList.add("cactus");
+    cactus.style.animation = `cacti ${cactusSpeed / 1000}s linear forwards`;
     gameContainer.appendChild(cactus);
 
-    setTimeout(function(){
+    setTimeout(function () {
         cactus.remove();
-    }, 2000);
+    }, cactusSpeed);
 }
 
-setInterval(function(){
+setInterval(function () {
     spawnCactus();
-}, Math.random() * 2000 + 1000);
+}, Math.random() * 200 + 1000);
 
-setInterval(function(){
+setInterval(function () {
     let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
     let cacti = document.querySelectorAll(".cactus");
 
@@ -43,15 +71,21 @@ setInterval(function(){
 
         if (cactusLeft < 150 && cactusLeft > 100 && dinoTop >= 140) {
             alert("You Have Lost");
-            if(Number(cur.textContent) > Number(high.textContent)){
-                high.textContent = cur.textContent
-                localStorage.setItem("high",Number(high.textContent))
-                counter = 0
-                cur.textContent = 0
-            }else{
-                counter = 0
-                cur.textContent = 0
+            if (Number(cur.textContent) > Number(high.textContent)) {
+                high.textContent = cur.textContent;
+                localStorage.setItem("high", Number(high.textContent));
             }
+            counter = 0;
+            cur.textContent = 0;
+        }
+    }
+}, 10);
+
+setInterval(() => {
+    if (counter >= 600) {
+        gameContainer.style.backgroundColor = "#fff";
+        if (counter >= 1200) {
+            gameContainer.style.backgroundColor = "#333";
         }
     }
 }, 10);
@@ -61,12 +95,3 @@ document.addEventListener("keyup", function (e) {
         jump();
     }
 });
-
-setInterval(()=>{
-    if(counter >= 600){
-        gameContainer.style.backgroundColor = "#fff"
-        if(counter >=1200){
-            gameContainer.style.backgroundColor = "#333"
-        }
-    }
-},20)
