3fa453ffaeee3770a2bc2559c16b24ddfc89c861
SabaBok <saba.bokuchava555@gmail.com>
2025-07-30 23:39:40 +0400

    commit

--- GOA Academy/Level131 Project/main/script.js
+++ GOA Academy/Level131 Project/main/script.js
@@ -2,16 +2,43 @@ const notisOpener = document.querySelector('.fa-bell')
 const notis = document.querySelector('#notis')
 let notisOpen = false
 
-notisOpener.addEventListener('click',e=>{
-    if(!notisOpen){
+const burger = document.querySelector('.burger')
+const blure = document.querySelector('#blur')
+const aside = document.querySelector('aside')
+let asideOpen = false
+
+//notification opener
+notisOpener.addEventListener('click', e => {
+    if (!notisOpen) {
         notis.style.display = 'flex'
         notisOpen = true
         notis.style.animation = 'NotisAppear 0.4s ease-in-out'
-    }else{
+    } else {
         notis.style.animation = 'NotisDissappear 0.4s ease-in-out'
         setTimeout(() => {
             notis.style.display = 'none'
             notisOpen = false
         }, 380);
+    }
+})
+
+//side-bar opener
+burger.addEventListener('click', e => {
+    if (!asideOpen) {
+        aside.style.transform = 'translateX(0)'
+        asideOpen = true
+        blure.style.display = 'block'
+        blure.style.animation = 'BlurAppear 0.3s ease-in-out'
+    }
+})
+//side-bar closer
+blure.addEventListener('click',e=>{
+    if(asideOpen){
+        blure.style.animation = 'BlurDissappear 0.3s ease-in-out'
+        aside.style.transform = 'translateX(-100%)'
+        setTimeout(() => {
+            blure.style.display = 'none'
+            asideOpen = false
+        }, 300);
     }
 })
