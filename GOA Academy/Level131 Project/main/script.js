f4bc9685b6447f38f1a49ee1e52da3b402e2cfff
SabaBok <saba.bokuchava555@gmail.com>
2025-07-31 00:59:24 +0400

    commit

--- GOA Academy/Level131 Project/main/script.js
+++ GOA Academy/Level131 Project/main/script.js
@@ -1,11 +1,48 @@
+const allAccs = JSON.parse(localStorage.getItem('finebank-accs')) || []
+const modal = document.querySelector('.modal')
+
+//notification
 const notisOpener = document.querySelector('.fa-bell')
 const notis = document.querySelector('#notis')
 let notisOpen = false
 
+//side-bar
 const burger = document.querySelector('.burger')
 const blure = document.querySelector('#blur')
 const aside = document.querySelector('aside')
 let asideOpen = false
+
+//creating card
+const addCard = document.querySelectorAll('.balance-create-card button')[0]
+const editCard = document.querySelectorAll('.balance-create-card button')[1]
+const cardWindow = document.querySelector('#add-card')
+const cardWindowForm = cardWindow.querySelector('form')
+let cardWindowOpen = false
+
+//card rendering
+function RenderAllCards() {
+
+}
+
+//modal creation
+let isModalRunning = false
+function modalAppear(text) {
+    const line = modal.querySelector('div')
+    if (!isModalRunning) {
+        isModalRunning = true
+        line.style.animation = 'modalTime 2.5s linear'
+        modal.style.animation = 'modalAppear 0.4s ease-in-out'
+        modal.style.opacity = '100%'
+        const modalText = modal.querySelector('p')
+        modalText.textContent = text
+        setTimeout(() => {
+            modal.style.opacity = '0'
+            modal.style.animation = 'none'
+            line.style.animation = 'none'
+            isModalRunning = false
+        }, 2600);
+    }
+}
 
 //notification opener
 notisOpener.addEventListener('click', e => {
@@ -31,9 +68,9 @@ burger.addEventListener('click', e => {
         blure.style.animation = 'BlurAppear 0.3s ease-in-out'
     }
 })
-//side-bar closer
-blure.addEventListener('click',e=>{
-    if(asideOpen){
+//side-bar closer // adding-card closer 
+blure.addEventListener('click', e => {
+    if (asideOpen) {
         blure.style.animation = 'BlurDissappear 0.3s ease-in-out'
         aside.style.transform = ''
         setTimeout(() => {
@@ -41,5 +78,49 @@ blure.addEventListener('click',e=>{
             asideOpen = false
             blure.style.animation = ''
         }, 300);
+    }if(cardWindowOpen){
+        blure.style.display = ''
+        cardWindow.style.display = ''
     }
+})
+
+//adding cards
+function CardTemp(fullName, accNumber, bank, cardType, branchName, money) {
+    this.fullName = fullName
+    this.accNumber = accNumber
+    this.bank = bank
+    this.cardType = cardType
+    this.branchName = branchName
+    this.money = money
+
+    this.goals = {}
+    bils = []
+    transactions = []
+    expenses = {}
+}
+addCard.addEventListener('click', e => {
+    blure.style.display = 'block'
+    cardWindow.style.display = 'block'
+    cardWindowOpen = true
+})
+cardWindowForm.addEventListener('submit',e=>{
+    e.preventDefault()
+    let fullname
+    const accNumber = e.target.accNumber.value
+    const bank = e.target.bank.value
+    const cardType = e.target.cardType.value
+    const branchName = e.target.branchName.value
+    const money = e.target.balance.value
+    if(accNumber.trim().length != 16 || accNumber.includes("abcdefghijklmnopqrstuvwxyz")){
+        modalAppear("enter a valid account number")
+        return  
+    }
+    
+    allAccs.forEach(el => {
+        if(el.logged && el.keepSigned){
+            fullname = el.fullName
+        }
+    })
+
+
 })
