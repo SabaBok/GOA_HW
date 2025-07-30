0da775c8f05b5ffb473134c954f12df461946a8e
SabaBok <saba.bokuchava555@gmail.com>
2025-07-30 22:24:17 +0400

    commit

--- GOA Academy/Level131 Project/main/script.js
+++ GOA Academy/Level131 Project/main/script.js
@@ -1,12 +1,17 @@
-const info = JSON.parse(localStorage.getItem('finebank-accs'))
-const idk = prompt('yes or no')
-if(idk == "yes"){
-    for(let i of info){
-        if(i.logged){
-            i.logged = false
-            i.keepSigned = false
-            window.location.href = '../index.html'
-        }
+const notisOpener = document.querySelector('.fa-bell')
+const notis = document.querySelector('#notis')
+let notisOpen = false
+
+notisOpener.addEventListener('click',e=>{
+    if(!notisOpen){
+        notis.style.display = 'flex'
+        notisOpen = true
+        notis.style.animation = 'NotisAppear 0.4s ease-in-out'
+    }else{
+        notis.style.animation = 'NotisDissappear 0.4s ease-in-out'
+        setTimeout(() => {
+            notis.style.display = 'none'
+            notisOpen = false
+        }, 380);
     }
-    localStorage.setItem('finebank-accs',JSON.stringify(info))
-}
+})
