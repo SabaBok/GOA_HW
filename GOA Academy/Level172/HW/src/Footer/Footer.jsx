f181bb9cc6c4eb5ad8a38803a4f0e28482bfcbe8
SabaBok <saba.bokuchava555@gmail.com>
2025-10-30 20:46:31 +0400

    product page fully finished

---
+++ GOA Academy/Level172/HW/src/Footer/Footer.jsx
@@ -0,0 +1,10 @@
+import Subscribe from "./Subscribe"
+import Ending from "./Ending"
+
+export default function Footer() {
+	return (
+		<footer className="w-full flex flex-col items-center self-end">
+			<Ending></Ending>
+		</footer>
+	)
+}
